433286d42987571251096f13eb92e77843271afd
mvLaboratory <mvLabaratory@gmail.com>
2016-03-09 22:40:18 +0200

    synchronizing whith master

--- src/com/mvLab/lab/accaunt/Main.java
+++ src/com/mvLab/lab/accaunt/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         MainWindow mainWindow = new MainWindow(primaryStage, labDB);
 
         mainWindow.Display();
+        //testing git
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 //        primaryStage.setTitle("Hello World");
 //        primaryStage.setScene(new Scene(root, 800, 500));
