c95fec30b85ef5c6c65a62dbce355f51a204ad17
mvLaboratory <mvLabaratory@gmail.com>
2016-03-13 16:45:16 +0200

    catalog element update

--- src/com/mvLab/lab/accaunt/Catalogs/Catalog.java
+++ src/com/mvLab/lab/accaunt/Catalogs/Catalog.java
@@ -1,22 +1,57 @@
 package com.mvLab.lab.accaunt.catalogs;
 
 import com.mvLab.lab.accaunt.DB_Manager;
+import com.mvLab.lab.accaunt.windows.WindowManager;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class Catalog {
+    private Integer id;
+    private String name;
     private UUID uuid;
+
+    public Catalog(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+        this.uuid = UUID.randomUUID();
+    }
+
+    public Catalog(Integer id, String name, String uuid) {
+        this.id = id;
+        this.name = name;
+
+        if (uuid == null || uuid.isEmpty())
+            this.uuid = UUID.randomUUID();
+        else
+            this.uuid = UUID.fromString(uuid);
+    }
 
     public Catalog() {
         this.uuid = UUID.randomUUID();
     }
 
-    public Catalog(String uuid) {
-        if (uuid == null || uuid.isEmpty())
-            this.uuid = UUID.randomUUID();
+    public Integer getId() {
+        if (id == null)
+            return id;
+        if (id == 0)
+            return null;
         else
-            this.uuid = UUID.fromString(uuid);
+            return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public UUID getUuid() {
@@ -30,6 +65,7 @@ public class Catalog {
     public void save() {
         //this.getClass().cast(this);
         DB_Manager.addReagentCatalogElement(this);
+        //TODO rewrite existing element
     }
 
     public void readElement() {
@@ -39,8 +75,22 @@ public class Catalog {
     public HashMap<String, Object> getElementFields() {
         HashMap<String, Object> fields = new HashMap<String, Object>();
 
-        //TODO fill the fields
+        ArrayList<Class> classes = new ArrayList<Class>();
+        classes.add(this.getClass());
+        //classes.add(element.getClass().getSuperclass());
 
+        for (Class catClass : classes) {
+            for (Field catFld : catClass.getDeclaredFields()) {
+                String fldName = catFld.getName();
+                fldName = fldName.substring(0, 1).toUpperCase() + fldName.substring(1);
+                try {
+                    fields.put(fldName, this.getClass().getMethod("get" + fldName).invoke(this));
+                } catch (Exception e) {
+                    //TODO Handle exception
+                    WindowManager.openErrorWindow(e.toString());
+                }
+            }
+        }
         return fields;
     }
 }
