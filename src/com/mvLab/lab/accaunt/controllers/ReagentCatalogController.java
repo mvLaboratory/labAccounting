365e8843028b27a1f186ff83e3d0d50ab4725b2b
mvLaboratory <mvLabaratory@gmail.com>
2016-08-29 08:30:00 +0300

    inner windows

--- src/com/mvLab/lab/accaunt/controllers/ReagentCatalogController.java
+++ src/com/mvLab/lab/accaunt/controllers/ReagentCatalogController.java
@@ -1,17 +1,19 @@
 package com.mvLab.lab.accaunt.controllers;
 
+import br.com.supremeforever.suprememdiwindow.MDICanvas;
+import br.com.supremeforever.suprememdiwindow.MDIWindow;
 import com.mvLab.lab.accaunt.catalogs.Catalog;
 import com.mvLab.lab.accaunt.WindowManager;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.util.Callback;
-
-import jfxtras.labs.scene.control.window.CloseIcon;
-import jfxtras.labs.scene.control.window.MinimizeIcon;
-import jfxtras.labs.scene.control.window.Window;
 
 
 
@@ -28,27 +30,27 @@ public class ReagentCatalogController<Type> implements EventHandler<MouseEvent>,
     @Override
     public void handle(MouseEvent event) {
         if (event.getClickCount() == 2) {
-            Type rowData = ((TableRow<Type>) event.getSource()).getItem();
-
-
-            Window w = new Window("My Window#");
-            w.setLayoutX(10);
-            w.setLayoutY(10);
-            // define the initial window size
-            w.setPrefSize(300, 200);
-            // either to the left
-            w.getLeftIcons().add(new CloseIcon(w));
-            // .. or to the right
-            w.getRightIcons().add(new MinimizeIcon(w));
-            w.getContentPane().getChildren().add(new Label("Content... \nof the window#"));
-            // add the window to the canvas
-            w.setStyle("view/DarkTheme.css");
-            //WindowManager.getInstance().getMainWindow().getPrimaryStage()
-
-
+            //Type rowData = ((TableRow<Type>) event.getSource()).getItem();
             //WindowManager.openReagentCatalogElementForm((Catalog) rowData);
+            MDICanvas canvas = new MDICanvas();
+            try {
+                int i = 0;
 
+                //canvas.setPrefSize(500, 500);
+                canvas.setAlignment(Pos.BASELINE_LEFT);
 
+                AnchorPane root = new AnchorPane();
+                Label label = new Label("sdsdsds");
+                root.getChildren().add(label);
+                MDIWindow mDIWindow = new MDIWindow("myUnicId" + i, new ImageView("http://icongal.com/gallery/icon/44614/32/signal_cell_sound_mobile_call_ringtone_phone"), "My Window Title " + i, root);
+                canvas.addMDIWindow(mDIWindow);
+                i++;
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            WindowManager.getInstance().getMainWindow().getRootLayout().getChildren().add(canvas);
         }
     }
 }
